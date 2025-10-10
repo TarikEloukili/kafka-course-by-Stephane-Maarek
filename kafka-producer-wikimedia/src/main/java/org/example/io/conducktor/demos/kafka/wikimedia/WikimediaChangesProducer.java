bd0929f3f62ff5bb8aa3950ef0b3a78ec5519ee3
TarikEloukili <tarikeloukili03@gmail.com>
2025-10-11 00:20:08 +0100

    run producer (note: don't forget to specify headers when sending get request to the url)

--- kafka-producer-wikimedia/src/main/java/org/example/io/conducktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/org/example/io/conducktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -9,6 +9,11 @@ import com.launchdarkly.eventsource.EventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import com.launchdarkly.eventsource.EventSource;
+import okhttp3.Headers;
+import java.net.URI;
+import java.time.Duration;
+
 public class WikimediaChangesProducer {
     public static void main(String[] args) throws InterruptedException {
 
@@ -25,14 +30,25 @@ public class WikimediaChangesProducer {
         properties.setProperty("value.serializer", StringSerializer.class.getName());
 
         // create the producer
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         String topic = "wikimedia.recentchange";
 
-        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
-        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
-        EventSource eventSource = builder.build();
+
+        Headers headers = new Headers.Builder()
+                .add("User-Agent", "Tarik-DE-Pipeline/1.0 (tarik@quantalgo.org)")
+                .add("Accept", "text/event-stream")
+                .build();
+
+
+        EventSource eventSource = new EventSource.Builder(
+                new WikimediaChangeHandler(producer, topic),
+                URI.create(url))
+                .headers(headers)
+                .reconnectTime(Duration.ofSeconds(3))   // polite reconnect/backoff
+                .build();
+
 
 
         // start the producer in another thread:
