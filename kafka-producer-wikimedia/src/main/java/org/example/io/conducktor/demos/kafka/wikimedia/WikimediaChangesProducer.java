accab48b6772b0896bbe9e3a1a4b95bedc9f8009
TarikEloukili <tarikeloukili03@gmail.com>
2025-10-10 14:06:57 +0100

    wikimedia producer project setup

---
+++ kafka-producer-wikimedia/src/main/java/org/example/io/conducktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,7 @@
+package org.example.io.conducktor.demos.kafka.wikimedia;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) {
+        System.out.println("Wikimedia changes producer");
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "Kafka-course-by-Stephane-Maarek"
+include("untitled")
+include("kafka-producer-wikimedia")
