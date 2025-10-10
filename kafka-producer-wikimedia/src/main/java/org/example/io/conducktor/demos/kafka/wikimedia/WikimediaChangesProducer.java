25a4cb04b656f1a4389e517af2664365e931c605
TarikEloukili <tarikeloukili03@gmail.com>
2025-10-10 23:47:24 +0100

    Wikimedia Producer Implementation

--- kafka-producer-wikimedia/src/main/java/org/example/io/conducktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/org/example/io/conducktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,45 @@
 package org.example.io.conducktor.demos.kafka.wikimedia;
 
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 public class WikimediaChangesProducer {
-    public static void main(String[] args) {
-        System.out.println("Wikimedia changes producer");
+    public static void main(String[] args) throws InterruptedException {
+
+        String bootstrapServer = "127.0.0.1:9092";
+
+        // create producer properties
+        Properties properties = new Properties();
+
+        // connect to localhost
+        properties.setProperty("bootstrap.servers", bootstrapServer);
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+
+        // start the producer in another thread:
+        eventSource.start();
+
+        // we produce for 10-min and block program until then
+        TimeUnit.MINUTES.sleep(10);
+
     }
 }
